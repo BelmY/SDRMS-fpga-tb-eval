af2f1791d275518d4536a69ad5b3f7c77dbe179f
drid <drid@stadar.org>
2019-08-28 16:10:22 +0300

    wrapper

---
+++ Vivado/LED Blink/LED Blink.srcs/sources_1/bd/design_1/synth/design_1.v
@@ -0,0 +1,36 @@
+//Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
+//Date        : Wed Aug 28 16:10:02 2019
+//Host        : drid-t420s running 64-bit Ubuntu 18.04.3 LTS
+//Command     : generate_target design_1.bd
+//Design      : design_1
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+(* CORE_GENERATION_INFO = "design_1,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=design_1,x_ipVersion=1.00.a,x_ipLanguage=VERILOG,numBlks=2,numReposBlks=2,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=0,numPkgbdBlks=0,bdsource=USER,da_board_cnt=4,da_clkrst_cnt=1,synth_mode=OOC_per_IP}" *) (* HW_HANDOFF = "design_1.hwdef" *) 
+module design_1
+   (LED1,
+    clk_100MHz,
+    reset_rtl_0);
+  output [15:0]LED1;
+  (* X_INTERFACE_INFO = "xilinx.com:signal:clock:1.0 CLK.CLK_100MHZ CLK" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME CLK.CLK_100MHZ, CLK_DOMAIN design_1_clk_100MHz, FREQ_HZ 100000000, INSERT_VIP 0, PHASE 0.000" *) input clk_100MHz;
+  (* X_INTERFACE_INFO = "xilinx.com:signal:reset:1.0 RST.RESET_RTL_0 RST" *) (* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME RST.RESET_RTL_0, INSERT_VIP 0, POLARITY ACTIVE_HIGH" *) input reset_rtl_0;
+
+  wire [15:0]c_counter_binary_0_Q;
+  wire clk_100MHz_1;
+  wire clk_wiz_clk_out1;
+  wire reset_rtl_0_1;
+
+  assign LED1[15:0] = c_counter_binary_0_Q;
+  assign clk_100MHz_1 = clk_100MHz;
+  assign reset_rtl_0_1 = reset_rtl_0;
+  design_1_c_counter_binary_0_0 c_counter_binary_0
+       (.CLK(clk_wiz_clk_out1),
+        .Q(c_counter_binary_0_Q));
+  design_1_clk_wiz_0 clk_wiz
+       (.clk_in1(clk_100MHz_1),
+        .clk_out1(clk_wiz_clk_out1),
+        .reset(reset_rtl_0_1));
+endmodule
